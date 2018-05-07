b36bfae4019c15bf1ccada9c4f69e8f16e1fb149
Nicolas Dunand <nico@kolya.ch>
2018-05-07 15:42:01 +0200

    Small refactoring for Moodle 3.5 release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Matrix question type
+
+## Original work
+
+Question type written by Penny Leach at Liip <penny@liip.ch>
+
+## Current work
+
+laurent.opprecht@unige.ch
+copyright 2011 University of Geneva
+
--- version.php
+++ version.php
@@ -1,8 +1,25 @@
-<?PHP
-/**
- * Question of type matrix
- */
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
 $plugin->component = 'qtype_matrix';
+$plugin->version   = 2018050700;
+
 $plugin->requires  = 2013040500;
+$plugin->release   = 'stable release for Moodle 2.7-3.5 (Build: 2018050700)';
+
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->version = 2017062900;
