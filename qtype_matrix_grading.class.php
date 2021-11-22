907bae43d1905809b5b87fffe987dee878822770
Nicolas Dunand <nico@kolya.ch>
2021-11-22 16:53:20 +0100

    Bugfix typo

--- qtype_matrix_grading.class.php
+++ qtype_matrix_grading.class.php
@@ -52,7 +52,7 @@ abstract class qtype_matrix_grading
         $class = 'qtype_matrix_grading_' . $type;
 
         require_once dirname(__FILE__) . '/grading/' . $class . '.class.php';
-        return $resut[$type] = call_user_func(array($class, 'create'), $type);
+        return $result[$type] = call_user_func(array($class, 'create'), $type);
     }
 
     public static function get_name()
